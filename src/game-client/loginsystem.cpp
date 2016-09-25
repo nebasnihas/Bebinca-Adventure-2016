8ed98ac4fbc66ce595b4fb4a4dd093d4338002bf
nnebas <nnebas@sfu.ca>
2016-09-25 16:03:21 -0700

    Save file now has additionl fields

--- src/game-client/loginsystem.cpp
+++ src/game-client/loginsystem.cpp
@@ -15,7 +15,7 @@ std::string username;
  password
  avatar type
  user level
- 
+ world
  */
 
 
@@ -24,11 +24,14 @@ std::string login();
 std::string _register();
 /*Returns the username for successful login/registration. Other methods can use the username to open game save file and modify/access it.*/
 bool save_file_exists (std::string user);
+void set_savefilevals(std::string user, std::string pass);
 
 int main(int argc, const char * argv[]) {
     
     welcomescreen();
-    return 1;
+    
+    std::cout << "This is the username returned " << username << std::endl;
+    return 0;
 }
 
 
@@ -48,11 +51,11 @@ void welcomescreen(){
     }
     
     std::cout << std::endl
-              << std::endl
-              << "Select an option" <<std::endl;
+    << std::endl
+    << "Select an option" <<std::endl;
     
     std::cout << "1. Login" << std::endl
-              << "2. Register\n";
+    << "2. Register\n";
     
     std::cin >> choice;
     
@@ -79,13 +82,13 @@ std::string login(){
     bool flag = false;
     
     while (flag == false){
-    std::cout << "Enter credentials" <<std::endl
-              << "Please enter username\n";
-    std::cin >> user;
-    
-    std::cout << "Please enter password\n";
-    std::cin >> pass;
-    
+        std::cout << "Enter credentials" <<std::endl
+        << "Please enter username\n";
+        std::cin >> user;
+        
+        std::cout << "Please enter password\n";
+        std::cin >> pass;
+        
         std::cout << save_file_exists(user +".txt"); //Remove this (for testing)
         
         if(!save_file_exists(user + ".txt")){
@@ -119,37 +122,50 @@ std::string _register(){
     std::string user, pass; // vars to store user input
     std::cout << "Create credentials" <<std::endl;
     bool flag = false;
-
+    
     
     while (flag == false){
-
+        
         std::cout << "Please select username\n";
         std::cin >> user;
         
         if(save_file_exists(user)){
             std::cout << "Username is already taken\n";
             flag = false;
+            
         } else {
             flag = true;
-            break;
+            
         }
         
     }
     std::cout << "Please input password\n";
     std::cin >> pass;
     
-    std::ofstream f;
-    f.open(user + ".txt");
-    f << user
-      << std::endl
-      << pass
-      << std::endl;
+    set_savefilevals(user, pass);
     
-    f.close();
     return user;
 }
 
 bool save_file_exists (std::string user){
     std::ifstream f(user);
     return f.good();
+}
+
+void set_savefilevals(std::string user, std::string pass){
+    
+    int user_level = 0;
+    std::string avatar = "nil";
+    std::string world = "start";
+    
+    std::ofstream f;
+    f.open(user + ".txt");
+    f << user << std::endl
+    << pass  << std::endl
+    << avatar << std::endl
+    << user_level << std::endl
+    << world << std::endl;
+    
+    f.close();
+    
 }
