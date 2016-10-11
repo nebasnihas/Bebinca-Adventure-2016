3e7dfbcf206c708f4942d41988411375d1e38603
Paul Madeya <pmadeya@sfu.ca>
2016-10-10 18:06:35 -0700

    Revised constructors and created initializer lists, created test constructor to test storing a room ID and room name

--- src/game-model/Area.hpp
+++ src/game-model/Area.hpp
@@ -11,26 +11,42 @@
 class Area 
 {
 
+	typedef std::unordered_map<std::string, std::string> Doors;
+
 public:
 
-	Area(std::string id, std::string areaName, std::unordered_map<std::string, std::string> connectedAreas, std::vector<std::string> description);
+	Area(std::string id, std::string title, Doors connectedAreas, std::vector<std::string> description);
 
-	std::string getID() const { return id; }
-	std::string getAreaName() const { return areaName; }
-    std::unordered_map<std::string, std::string>* getConnectedAreas() const { return (std::unordered_map<std::string, std::string>*)&connectedAreas; }
+	//for testing
+	Area(std::string id, std::string title);
+
+	std::string getID() const;
+	std::string getTitle() const;
+	std::vector<std::string> getDescription() const;
+	Doors* getConnectedAreas() const;
+    
+
+
+
     std::vector<Entity> getEntityList() const { return entityList; }
 
-	std::string getAreaDescription() const;
+	// std::string getAreaDescription() const;
+
+
+
 
     //TO-DO: Add loading mechanism from file
     static Area* loadAreaFromFile(std::string);
 
 private:
     // Id of the area object, may not be needed depending on networking implementation
-	std::string id;
-    std::string areaName;
-    std::unordered_map<std::string, std::string> connectedAreas;
 
+
+	std::string id;
+    std::string title;
+	Doors connectedAreas;
+
+    std::vector<std::string> description;
 	std::vector<Entity> entityList;
 
 	//TO-DO: Add more detail to an area
