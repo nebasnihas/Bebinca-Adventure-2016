3089251ea0d841cefacb91c80d3b2816b0067d31
Paul Madeya <pmadeya@sfu.ca>
2016-10-23 12:41:40 -0700

    Restructured member functions and private fields in Entity class, built constructor, and getter functions

--- src/game-model/Entity.hpp
+++ src/game-model/Entity.hpp
@@ -2,23 +2,41 @@
 #define ENTITY_HPP
 
 #include <string>
+#include <vector>
 
 class Entity {
 
 public:
 
-    Entity(std::string entityID, std::string name, std::string description) : entityID{entityID}, displayName{displayName}, description{description} {}
+    Entity(std::vector<std::string> attributes, int cost, std::vector<std::string> extra, std::string ID, std::string type, std::vector<std::string> keywords, std::string description, std::string name, std::vector<std::string> wearflags, int weight);
 
-    std::string getEntityID() const { return entityID; }
-    std::string getDisplayName() const { return displayName; }
-    std::string getDescription() const { return description; }
+
+    std::string getID();
+    std::string getDescription();
+    std::vector<std::string> getAttributes();
+	int getCost();
+	std::vector<std::string> getExtra();
+    std::string getType(); //Maybe we can have a subclass for each type of object
+    std::vector<std::string> getKeywords();
+    std::string getName();
+    std::vector<std::string> getWearflags();
+    int getWeight();
+
+
 
 private:
 
-    std::string entityID;
-    std::string displayName;
+	std::vector<std::string> attributes;
+	int cost;
+	std::vector<std::string> extra;
+    std::string ID;
+    std::string type; //Maybe we can have a subclass for each type of object
+    std::vector<std::string> keywords;
     std::string description;
-
+    std::string name;
+    std::vector<std::string> wearflags;
+    int weight;
+    
 };
 
 #endif
