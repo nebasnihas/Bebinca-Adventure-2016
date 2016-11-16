93c680599ff0a4dc9e3a83794a31a83745ffa071
Zachary Cesaretti <zcesaret@sfu.ca>
2016-11-16 12:50:24 -0800

    Implemented ability to build YAML node given room inputs

--- include/game/YamlAreaBuilder.hpp
+++ include/game/YamlAreaBuilder.hpp
@@ -8,16 +8,15 @@
 #include <assert.h>
 #include <fstream>
 #include "../../lib/yaml-cpp/src/nodebuilder.h"
+#include "yaml-cpp/yaml.h"
 
 class YamlAreaBuilder
 {
 public:
 
-    //static void createFile(const std::string& fileName);
-
-    static YAML::Node* populateAreasNode(std::vector<std::string> desc, std::vector<std::string> extended_descriptions, std::string id, std::string name, std::vector<std::string> doorDesc, std::vector<std::string> keywords, std::string doorDir, std::string doorTo);
-
-    //static std::string getFilePath(const std::string& fileName);
+    static YAML::Node populateAreasNode(std::vector<std::string> desc, std::vector<std::string> extended_descriptions,
+                                        std::string id, std::string name, std::vector<std::string> doorDesc,
+                                        std::vector<std::string> keywords, std::string doorDir, std::string doorTo);
 
 private:
 
--- src/game-model/YamlAreaBuilder.cpp
+++ src/game-model/YamlAreaBuilder.cpp
@@ -5,24 +5,39 @@
 //
 //}
 
-YAML::Node* YamlAreaBuilder::populateAreasNode(std::vector<std::string> desc, std::vector<std::string> extended_descriptions, std::string id, std::string name, std::vector<std::string> doorDesc, std::vector<std::string> keywords, std::string doorDir, std::string doorTo)
+YAML::Node YamlAreaBuilder::populateAreasNode(std::vector<std::string> desc, std::vector<std::string> extended_descriptions,
+                                              std::string id, std::string name, std::vector<std::string> doorDesc,
+                                              std::vector<std::string> keywords, std::string doorDir, std::string doorTo)
 {
     //top level rooms node
-    YAML::Node *ROOMS;
+    YAML::Node ROOMS;
 
-    ROOMS*["desc"] = desc;
-    ROOMS["extended_descriptions"] = extended_descriptions;
-    ROOMS["id"] = id;
-    ROOMS["name"] = name;
+    ROOMS["desc"].push_back(desc);
+    ROOMS["extended_descriptions"].push_back(extended_descriptions);
+    ROOMS["id"].push_back(id);
+    ROOMS["name"].push_back(name);
 
-    ROOMS["doors"]["dir"] = doorDir;
-    ROOMS["doors"]["to"] = doorTo;
-    ROOMS["doors"]["desc"] = doorDesc;
-    ROOMS["doors"]["keywords"] = keywords;
+    ROOMS["doors"]["dir"].push_back(doorDir);
+    ROOMS["doors"]["to"].push_back(doorTo);
+    ROOMS["doors"]["desc"].push_back(doorDesc);
+    ROOMS["doors"]["keywords"].push_back(keywords);
 
     return ROOMS;
 }
 
+//////////////////////Try #2
+
+//    ROOMS*["desc"] = desc;
+//    ROOMS["extended_descriptions"] = extended_descriptions;
+//    ROOMS["id"] = id;
+//    ROOMS["name"] = name;
+//
+//    ROOMS["doors"]["dir"] = doorDir;
+//    ROOMS["doors"]["to"] = doorTo;
+//    ROOMS["doors"]["desc"] = doorDesc;
+//    ROOMS["doors"]["keywords"] = keywords;
+
+///////////////////////Try #1
 
 //
 //    YAML::Node descNode;
