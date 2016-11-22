fd82fee814a2e4b651e01c8fc0ea04b5030712cf
nnebas <nnebas@sfu.ca>
2016-11-21 22:53:13 -0800

    Redefined function params to take in Window and x,y coordinates, changed color codes

--- src/game-client/StrColorizer.hpp
+++ src/game-client/StrColorizer.hpp
@@ -13,55 +13,56 @@
 #include <string>
 #include <vector>
 #include <memory>
+#include <ncurses.h>
 
 //Standard ncurses colors
 enum class color_type: char {
-    
-    BLACK   = 'b',
+
+    BLACK   = 'l',
     RED     = 'r' ,
     GREEN   = 'g' ,
     YELLOW  = 'y',
-    BLUE    = 'l',
+    BLUE    = 'b',
     MAGENTA = 'm',
     CYAN    = 'c',
     WHITE   = 'w'
-    
+
 };
-    
+
 struct color_token {
-    
+
     char character;
     color_type charcolor;
     //Each character has a character color attribute
-    
+
 };
-    
+
 typedef std::vector<color_token> attribute_string;
 
 class StrColorizer {
-    
+
 private:
     static bool is_tag(char c);
     static color_type get_color(char c);
     static bool is_space(char c);
-    
-   friend std::ostream& operator<<(std::ostream& os, const attribute_string& att_str);
-    
-    
-    
+
+    friend std::ostream& operator<<(std::ostream& os, const attribute_string& att_str);
+
+
+
     //-------------Converter-----------------//
     static attribute_string str_to_colortoken (const std::string& text);
-    
+
     //-------------Printer-------------------//
-    static void colortoken_print (const attribute_string& att_str);
-    
-    
+    static void colortoken_print (WINDOW *win, int ypos, int xpos,const attribute_string& att_str);
+
+
 public:
-    
-    static void print_color(const std::string& text);
-    
-    
+
+    static void print_color(WINDOW *win, int ypos, int xpos, const std::string& text);
+
+
 };
-    
+
 
 #endif /* parse_hpp */
